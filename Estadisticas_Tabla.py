767741e302f6b40bbae3983236d0a6f9117cdb98
fernando.gonzalez <pachecofgf@gmail.com>
2015-10-22 22:12:51 -0500

    cambios

--- Estadisticas_Tabla.py
+++ Estadisticas_Tabla.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
 __author__ = 'fernando.gonzalez'
-import arcpy, os, sys
+import os
+import sys
+import arcpy
+import datetime
 # importar utf8
 reload(sys)
 sys.setdefaultencoding("utf-8")
 #Parametros
-
+arcpy.env.overwriteOutput=True
 tablaEntrada=r"X:\PRUEBAS\ReportePagos_2015\Datos_Prueba.gdb\DATOS_PRUEBA"
 tablaEstadisticas=r"X:\PRUEBAS\ReportePagos_2015\Datos_Prueba.gdb\Estadisticas"
 
@@ -23,38 +27,112 @@ def calcularDescripcion(tabla,dominio,campo):
             rowsup.updateRow(rowup)
     arcpy.Delete_management("in_memory/dominioOut")
 
-def TablaArray(table):
+def TablaArray(table,fields):
         array=[]
         rows = arcpy.SearchCursor(table)
-        listFields = arcpy.ListFields(table)
+        encabezado=[]
+        for field in fields:
+            if field == "ACTIVIDAD":
+                encabezado.append("Actividad Relacionada Segun Contrato")
+            if field == "SUM_AREA":
+                encabezado.append("Area HA")
+            if field == "SUM_AREA":
+                encabezado.append("Area HA")
+
+        array.append(field)
         for row in rows:
             arrayrow=[]
-            for field in listFields:
-                arrayrow.append(row.getValue(field.name))
+            for field in fields:
+                arrayrow.append(row.getValue(field))
             array.append(arrayrow)
         return array
 
 camposSum= [["AREA","SUM"],["VALOR_PAGADO","SUM"]]
-camposUnicos= ["ESCALA","ACTIVIDAD","CONTRATISTA"]
-
+camposUnicos= ["ESCALA","ACTIVIDAD","PROYECTO"]
+fields=["ACTIVIDAD","PROYECTO","ESCALA","SUM_AREA","SUM_VALOR_PAGADO"]
 arcpy.Statistics_analysis(tablaEntrada, tablaEstadisticas, camposSum, camposUnicos)
 calcularDescripcion(tablaEstadisticas,"DOM_PAGOS_ESCALA","ESCALA")
 calcularDescripcion(tablaEstadisticas,"Dom_Actividad_Pagos","ACTIVIDAD")
+calcularDescripcion(tablaEstadisticas,"Dom_Proyecto_Pagos","PROYECTO")
 
-data=TablaArray(tablaEstadisticas)
+data=TablaArray(tablaEstadisticas,fields)
 
-from reportlab.pdfgen import canvas
-from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.platypus import Paragraph, SimpleDocTemplate, Spacer, Table
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.enums import TA_CENTER,TA_JUSTIFY,TA_LEFT,TA_RIGHT
+from reportlab.lib.units import inch
+from reportlab.lib import colors
+
+
+####################
 width, height = letter
-doc = SimpleDocTemplate("X:\PRUEBAS\PDF\simple_table_grid3.pdf", pagesize=letter)
+doc = SimpleDocTemplate("X:\PRUEBAS\PDF\simple_table_grid24.pdf", pagesize=letter)
 elements = []
-t=Table(data,style=[('GRID',(1,1),(-2,-2),1,colors.black),
-                ('BOX',(0,0),(1,-1),2,colors.black),
-                ('LINEABOVE',(1,2),(-2,2),1,colors.black),
-                ('FONTSIZE',(5)),
-                ('LINEBEFORE',(2,1),(2,-2),1,colors.black),
+#Alineacion
+
+styles=getSampleStyleSheet()
+styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+styles.add(ParagraphStyle(name='Left', alignment=TA_LEFT))
+styles.add(ParagraphStyle(name='Center', alignment=TA_CENTER))
+styles.add(ParagraphStyle(name='Right', alignment=TA_RIGHT))
+
+# FECHA
+
+fecha= datetime.date.today()
+hoy= "Bogotá "+fecha.strftime("%d/%m/%Y")
+
+fechapara=Paragraph(hoy, styles["Left"])
+elements.append(fechapara)
+
+elements.append(Spacer(0, inch*.3))
+
+# creamos título con estilo
+titulo = Paragraph("INFORME DE PRODUCCIÓN",styles["Center"])
+elements.append(titulo)
+# Contrato
+contrato = "12245-2013"
+contratotxt = Paragraph("CONTRATO No. <strong>%s</strong>"%(contrato),styles["Center"])
+elements.append(contratotxt)
+#Periodo
+periodo = "2015-01-01 al 2015-02-01"
+periodotxt=Paragraph("Periódo a Reportar <strong>%s</strong>"%(periodo),styles["Center"])
+elements.append(periodotxt)
+
+elements.append(Spacer(0, inch*.3))
+
+actividadesTitulo =Paragraph("<strong>ACTIVIDADES DEL CONTRATO</strong>",styles["Right"])
+# espacio adicional
+elements.append(Spacer(0, inch*.1))
+
+def actividades(Actividad):
+    texActividades=[]
+    if Actividad=="Control Restitucion":
+        texActividades.append("Obligaciones del contratista:")
+        texActividades.append("2.1- Validar en estación digital la correcta captura de los elementos altimétricos y planimétricos o modelos digitales del terreno,obtenidos mediante restitución \
+                               por funcionarios y contratistas del área, a diferentes escalas, de acuerdo con las asignaciones y plan del trabajo del área técnica.")
+        texActividades.append("2.2- Verificar la consistencia temática, consistencia lógica, la totalidad de la información. La correcta posición y los empalmes y conectividad entre niveles.")
+        texActividades.append("2.3- Diligenciar los formatos respectivos del control  del control de calidad realizando a cada proyecto.")
+        texActividades.append("2.4- Cumplir con los rendimientos en producción establecidos por la subdirección de geografía y cartografía.")
+        texActividades.append("2.5- Realizar el diagnostico de cada hoja cartográfica asignada y aceptarla si cumple con el 90% de las especificaciones.")
+    return texActividades
+
+
+for text in actividades("Control Restitucion"):
+    para = Paragraph(text, styles["Justify"])
+    elements.append(para)
+
+
+# espacio adicional
+elements.append(Spacer(0, inch*.2))
+
+
+
+
+t=Table(data,style=[
+                ('BOX', (0,0), (-1,-1), 0.25, colors.black),
+                ('FONTSIZE', (0,0), (-1, -1),4),
+                ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
                 ])
 
 elements.append(t)
